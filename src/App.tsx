dc074e89207c021b8636c5f8f2abe6e839cdf66b
binAryalChhetri <binayakchhetri1234@gmail.com>
2025-01-30 23:49:16 +0545

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BlogPost } from "./modules/blog-post/components/BlogPost";
 
 export default function App() {
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-6xl mx-auto p-5">
       <Header />
       <BlogPost />
     </div>
